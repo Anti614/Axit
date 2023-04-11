da692d52bf72007babd5a7526e96c289fb0b46f9
Anti614 <julianikulina55@gmail.com>
2023-04-10 20:38:54 -0600

    update

--- js/main.js
+++ js/main.js
@@ -1,12 +1,33 @@
-$(document).ready(function(){
-    let nav = $("#navbarContent");
-    $(".navbar a").click(function() {
-          
-        $("body,html").animate({
-         scrollTop:$("#" + $(this).data('value')).offset().top - $("#header").height()
-        }, 700);
-        nav.removeClass("show");
+$(document).ready(function () {
+  let nav = $("#navbarContent");
+  $(".navbar a").click(function () {
 
-    });
-     
+    $("body,html").animate({
+      scrollTop: $("#" + $(this).data('value')).offset().top - $("#header").height()
+    }, 700);
+    nav.removeClass("show");
+
+  });
+
+  
+  const cards = document.querySelectorAll('.product__card');
+  cards.forEach((card) => {
+    card.addEventListener('click', cardClick);
+  });
+
+  function cardClick(event) {
+    let p = event.currentTarget?.querySelector('.product__ad');
+
+    if (!p) {
+      return;
+    }
+
+    if (p.style.display == 'none') {
+      p.style.display = 'block';
+    } else {
+      p.style.display = 'none';
+    }
+  }
+
 });
+
